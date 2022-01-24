26378dd3fc2a9e6cc87863a02de77bd749f7e12f
Nockyo <95856198+Nockyo@users.noreply.github.com>
2022-01-24 12:10:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,23 @@ const firstName = 'Guyard';
 
 const helloWorld = <React.Fragment><h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1></React.Fragment>
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+// ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+function MonSuperComposant() {
+
+    return(
+        <section>
+            <article className="article">
+                <p>hello10</p>
+            </article>
+            <article className="article">
+                <p>hello20</p>
+            </article>
+            <article className="article">
+                <p>hello30</p>
+            </article>
+        </section>
+    )
+}
+
+ReactDOM.render(<MonSuperComposant />, document.querySelector('#app'));
