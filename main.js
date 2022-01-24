6404a611d8b475209801c5c9c60a4280815e8bc0
Nockyo <95856198+Nockyo@users.noreply.github.com>
2022-01-24 12:05:57 +0100

    ex03

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const helloWorld = <h1>Hello world!</h1>;
+
+const lastName = 'Mathieu';
+const firstName = 'Guyard';
+
+const helloWorld = <React.Fragment><h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1></React.Fragment>
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
