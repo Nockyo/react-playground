1130fded777e71e53da2d2ed6efcc87842afd0c0
Nockyo <95856198+Nockyo@users.noreply.github.com>
2022-01-25 15:11:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,86 @@
-function UserGreeting(props) {
-    return <h1>Bienvenue !</h1>;
-  }
+// function UserGreeting(props) {
+//     return <h1>Bienvenue !</h1>;
+//   }
   
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
-  }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+//   function GuestGreeting(props) {
+//     return <h1>Veuillez vous connecter</h1>;
+//   }
+//   function Greeting(props) {
+//       const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
-      }
+//       const handleLogIn = (e) => {
+//           e.preventDefault();
+//           setIsLoggedIn(true);
+//       }
 
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
+//       const handleLogOut = (e) => {
+//           e.preventDefault();
+//           setIsLoggedIn(false);
+//       }
 
-    return(
+//     return(
+//         <React.Fragment>
+//             {isLoggedIn ? 
+//             <React.Fragment>
+//                 <UserGreeting />
+//                 <button onClick={handleLogOut}>Se déconnecter</button>
+//             </React.Fragment> : 
+//             <React.Fragment>
+//                 <GuestGreeting />
+//                 <button onClick={handleLogIn}>Se connecter</button>
+//             </React.Fragment> }
+//         </React.Fragment>
+//     )
+//   }
+  
+//   ReactDOM.render(
+//     <Greeting />,
+//     document.querySelector('#app')
+//   );
+
+function App(props){
+
+    const [state, setState] = React.useState({
+        items: [],
+        DataIsLoaded: false
+    });
+
+    const { DataIsLoaded, items } = state;
+    
+    React.useEffect(() => {
+        fetch(
+            "https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((json) => {
+                setState({
+                    items: json,
+                    DataIsLoaded: true
+                });
+            })
+    })
+        
+    return (
         <React.Fragment>
-            {isLoggedIn ? 
+            {!DataIsLoaded ?
             <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
+                <h1> Please wait some time... </h1> 
+            </React.Fragment> :
             <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
+                <h1> Fetch data from an api in react </h1> 
+                {
+                    items.map((item) => (
+                    <ol key = { item.id } >
+                        User_Name: { item.username },<br/>
+                        User_Email: { item.email },<br/>
+                        User_Society: { item.company.name},<br/>
+                        User_PhoneNumber: {item.phone},<br/>
+                        User_Website: {item.website}
+                    </ol>
+                    ))
+                }
+            </React.Fragment>}
         </React.Fragment>
-    )
-  }
-  
-  ReactDOM.render(
-    <Greeting />,
-    document.querySelector('#app')
-  );
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
